4f7b77cd3eb114c956f327720ce67b122e12195a
yavuzhan66 <gtacinq45@gmail.com>
2023-10-05 22:59:54 +0200

    new

--- frontend/homepage/script.js
+++ frontend/homepage/script.js
@@ -1,3 +1,5 @@
+
+////////////////////////POPUP//////////////////:
 function togglePopup() {
     var popup = document.getElementById("popup-1");
     popup.classList.toggle("active");
@@ -10,3 +12,19 @@ function closePopup() {
 
 
 
+/////////////////////BT-UP/////
+const btUp = document.querySelector('#bt-up');
+btUp.style.display = 'none';
+btUp.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+})
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 150) {
+        btUp.style.display = 'flex';
+    } else {
+        btUp.style.display = 'none';
+    }
+})
